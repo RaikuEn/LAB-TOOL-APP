6fb91554774d4e67b23deae16376b475f4c0a0cd
RaikuEn <hasan.arwi.nurzaman@gmail.com>
2026-03-09 13:15:51 +0700

    user update

--- app.js
+++ app.js
@@ -128,6 +128,14 @@ app.post('/add-tool', verifyToken, isAdmin, async (req, res) => {
   }
 });
 
+app.get('/profile', verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).send("Error fetching profile");
+  }
+});
 
 // Search tools by name or category
 app.get('/search', async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }, // We will hash this!
-  role: { type: String, default: 'user' } // New: 'admin' or 'user'
+  role: { type: String, default: 'user' }, // New: 'admin' or 'user'
+  fullName: { type: String, required: true },
+  indexNumber: { type: String, required: true, unique: true }, // Different from MongoDB _id
+  email: { type: String, required: true, unique: true },
+  className: { type: String, required: true } // "Class" is a reserved word in JS, so use "className"
 });
 module.exports = mongoose.model('User', UserSchema);
